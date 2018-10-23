991daf056ba6e256cb9e2344de207b273a94a30a
expelthegrace <32312668+expelthegrace@users.noreply.github.com>
2018-10-23 14:50:26 +0200

    creant ranking
    menu navegable, options no fet. ranking falta filtrar/ordenar. play no fet

---
+++ FlappyBird2D/Assets/Scripts/Menu/menucontroller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menucontroller : MonoBehaviour {
+
+    public GameObject mainPage;
+    public GameObject rankingPage;
+    public GameObject optionsPage;
+
+    
+
+    // Use this for initialization
+    void Start () {
+        mainPage.SetActive(false);
+        rankingPage.SetActive(true);
+        optionsPage.SetActive(false);
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void switchPages(int x)
+    {
+        switch(x){
+            case 0: // main to ranking
+                mainPage.SetActive(false);
+                rankingPage.SetActive(true);
+                break;
+            case 1: // ranking back to main
+                rankingPage.SetActive(false);
+                mainPage.SetActive(true);
+                break;
+            case 2: // main to options
+                optionsPage.SetActive(true);
+                mainPage.SetActive(false);
+                break;
+            case 3: // options back to main
+                optionsPage.SetActive(false);
+                mainPage.SetActive(true);
+                break;
+        }
+
+    }
+
+}
---
+++ FlappyBird2D/Assets/Scripts/Menu/scrollRanking.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class scrollRanking : MonoBehaviour {
+
+    struct entrada
+    {
+        public string nom;
+        public int score;
+
+        public entrada(string nom, int score)
+        {
+            this.nom = nom;
+            this.score = score;
+        }
+    }
+
+    public Text rankingT;
+    private List<entrada> entrades;
+
+	// Use this for initialization
+	void Start () {
+        entrades = new List<entrada>();
+        rankingT.text = "";
+        for (int i = 0; i < 100; ++i)
+        {
+            entrada entr = new entrada("PlayerName", 9999 );
+            rankingT.text = rankingT.text + (i+1).ToString() + ": " + entr.nom + " - " + entr. score + "\n";
+        }
+
+        
+	}
+	
+    void listToString()
+    {
+        rankingT.text = "";
+        for (int i = 0; i < entrades.Count; ++i) rankingT.text = rankingT.text + (i+1).ToString() + entrades[i].nom + " - " + entrades[i].score + "\n";
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
